52fbe2e6a5caf6ae980e7b89634c51a1d4269839
Ethan Capouillez <ethan.capouillez@hotmail.be>
2022-06-11 17:09:27 +0200

    i

---
+++ src/main/java/be/unpainperdu/celte/util/ModTags.java
@@ -0,0 +1,34 @@
+package be.unpainperdu.celte.util;
+
+import be.unpainperdu.celte.Celte;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public class ModTags {
+    public static class Blocks {
+        public static final TagKey<Block> OLD_OAK_LOGS = tag("old_oak_logs");
+
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(new ResourceLocation(Celte.MOD_ID, name));
+        }
+
+        private static TagKey<Block> forgeTag(String name) {
+            return BlockTags.create(new ResourceLocation("forge", name));
+        }
+    }
+
+    public static class Items {
+
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(new ResourceLocation(Celte.MOD_ID, name));
+        }
+
+        private static TagKey<Item> forgeTag(String name) {
+            return ItemTags.create(new ResourceLocation("forge", name));
+        }
+    }
+}
